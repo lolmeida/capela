9f2d13030f19a9bb69936448d4c3fe1805766dd0
lolmeida <39884938+lolmeida@users.noreply.github.com>
2023-12-04 23:20:26 +0000

    Update Receipt.java

--- src/main/java/com/lolmeida/entity/database/Receipt.java
+++ src/main/java/com/lolmeida/entity/database/Receipt.java
@@ -1,1 +1,46 @@
+package com.lolmeida.entity.database;
+
+
+import com.lolmeida.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.List;
+
+
+@Getter
+@Setter
+@Slf4j
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+
+@Entity
+@Table(name = "tRecebimentos") //, schema = "sql7509759")
+public class Receipt extends BaseEntity {
+    @Id
+    @Column(name = "Chave")
+    private String id;
+
+    @Column(name = "Outros")
+    private double otherAmount;
+
+    @Column(name = "ValorPago")
+    private double amount;
+
+
+
+    @ManyToOne
+    @JoinColumn(name = "cargoNumber")
+    private Cargo cargo;
+
+    @ManyToOne
+    @JoinColumn(name = "clientId")
+    private Client client;
+
+    @OneToMany(mappedBy = "cargoNumber", fetch = FetchType.LAZY)
+    private List<Cargo> sizeList;
+
+}
 
